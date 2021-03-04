1f58bd6b0f8c9b17592b472b5397542ea76139b4
juliaabad <juliaab93@gmail.com>
2021-03-04 10:39:13 +0100

    instalados modulos angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { Componente1Component } from './components/componente1/componente1.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuSe-4yl_zWDdbMws1jUFjpIq39mHjZIs",
+  authDomain: "angular-course-bed39.firebaseapp.com",
+  projectId: "angular-course-bed39",
+  storageBucket: "angular-course-bed39.appspot.com",
+  messagingSenderId: "879727463548",
+  appId: "1:879727463548:web:dc28f6b9a1e1c1badacdf9",
+  measurementId: "G-5TQ57ZQ618"
+};
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    // 3. Initialize
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
+  ],
+  declarations: [ AppComponent, Componente1Component ],
+  bootstrap: [ AppComponent ]
+})
+
 @NgModule({
   declarations: [
     AppComponent
